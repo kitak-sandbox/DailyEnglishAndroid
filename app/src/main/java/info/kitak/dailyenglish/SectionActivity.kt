7305525ea7f270a0e10ad3065d6d80564b5dc741
Keisuke KITA <kei.kita2501@gmail.com>
2017-04-09 12:31:50 +0900

    Initial commit

---
+++ app/src/main/java/info/kitak/dailyenglish/SectionActivity.kt
@@ -0,0 +1,12 @@
+package info.kitak.dailyenglish
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_section)
+    }
+}
